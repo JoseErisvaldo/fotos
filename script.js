c0ed27444026e70139e2edf4e003d1f600e2cf22
JoseErisvaldo <100242848+JoseErisvaldo@users.noreply.github.com>
2023-01-20 19:34:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+clicar.addEventListener('click', um)
+
+function um () {
+    let um = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-1.jpg'> "
+}
+
+clicar.addEventListener('click', dois)
+
+function dois () {
+    let dois = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-2.jpg'> "
+}
+clicar.addEventListener('click', tres)
+
+function tres () {
+    let tres = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-3.jpg'> "
+}
+
+clicar.addEventListener('click', quatro)
+
+function quatro () {
+    let quatro = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-4.jpg'> "
+}
+
+clicar.addEventListener('click', cinco)
+
+function cinco () {
+    let cinco = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-5.jpg'> "
+}
+
+clicar.addEventListener('click', seis)
+
+function seis () {
+    let seis = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-6.jpg'> "
+}
+
+clicar.addEventListener('click', sete)
+
+function sete () {
+    let sete = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-7.jpg'> "
+}
+
+clicar.addEventListener('click', oito)
+
+function oito () {
+    let oito = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-8.jpg'> "
+}
+
+clicar.addEventListener('click', nove)
+
+function nove () {
+    let nove = document.getElementById('conteudo-esquerda').innerHTML = " <img src='./imagens/imagem-9.jpg'> "
+}
+
